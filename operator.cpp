44f637d2e4297101a4c24ac402442dcb8d857d53
swarup Goswami <goswamiswarup97@gmail.com>
2026-01-31 19:46:55 +0530

    done with operator part

---
+++ operator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cout<<"enter the value of a and b"<<endl;
+    cin>>a>>b;
+    int sum=a+b;
+    cout<<"multiplication is"<<(a*b)<<endl;
+    cout<<"substraction is"<<(a-b)<<endl;
+    cout<<"sum is :"<<sum<<endl;
+    return 0;
+}

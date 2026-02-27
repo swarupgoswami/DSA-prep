8a06cc0061bd64819c5869276f626473ef150d2f
swarup Goswami <goswamiswarup97@gmail.com>
2026-02-27 18:26:13 +0530

    binarsearch

--- leetcode/binarysearch.cpp
+++ leetcode/binarysearch.cpp
@@ -2,9 +2,37 @@
 #include<vector>
 using namespace std;
 
+int binearySearch(vector<int> nums,int t){
+     int start=0;
+     int n=nums.size();
+     int end=n-1;
+     int mid;
+
+    while(start <= end){
+        mid=(start+end)/2;
+        if(t > nums[mid]){
+            start=mid+1;
+
+        }
+        else if(t < nums[mid]){
+              end=mid-1;
+        }
+        else{
+           return mid;
+        }
+    }
+    return -1;
+
+}
+
 
 
 
 int main(){
+
+    vector<int> nums={1,2,3,4,5,6,7,8,9};
+    int bs=binearySearch(nums,9);
+    cout<<bs<<endl;
+    return 0;
   
 }

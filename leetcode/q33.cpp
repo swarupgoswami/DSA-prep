952e58ca9c01615a62ba14176e357a7f3880242f
swarup Goswami <goswamiswarup97@gmail.com>
2026-02-27 21:06:05 +0530

    binar search in reverse sorted array

---
+++ leetcode/q33.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int search(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+
+        while(start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] == target)
+                return mid;
+
+            // Left half sorted
+            if(nums[start] <= nums[mid]) {
+
+                if(target >= nums[start] && target < nums[mid])
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+            // Right half sorted
+            else {
+
+                if(target > nums[mid] && target <= nums[end])
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+        }
+
+        return -1;
+}    
+
+
+
+
+int main(){
+    vector<int>nums={3,4,5,6,7,0,1,2};
+    int bs= search(nums,6);
+    cout<<bs<<endl;
+
+}

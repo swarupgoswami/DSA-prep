78103bdbb8b048e276af087c61cb7185ec44918c
swarup Goswami <goswamiswarup97@gmail.com>
2026-06-20 00:41:00 +0530

    minimum in a rotated sorted array

---
+++ leetcode/q153.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int minimuminbinarysearchsortedarray(vector<int>&nums){
+    int start=0;
+    int end=nums.size()-1;
+    while(start<end){
+        int mid=start+(end-start)/2;
+
+        
+
+        if(nums[mid]>nums[end]){
+            start=mid+1;
+        }
+        else{
+            end=mid;
+        }
+
+    }
+    return nums[start];
+
+}
+
+
+int main(){
+    vector<int>nums={4,5,6,7,1,2};
+    int minimum=minimuminbinarysearchsortedarray(nums);
+    cout<<"the mimimum number is "<<minimum<<endl;
+    return 0;
+    
+
+}

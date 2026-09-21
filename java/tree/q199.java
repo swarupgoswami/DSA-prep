d0367295a79ecd4042ad780eaf0d70b1de3c0ad3
swarup Goswami <goswamiswarup97@gmail.com>
2026-09-21 20:25:49 +0530

    q199 subhadeep swarup subhadeep

---
+++ java/tree/q199.java
@@ -0,0 +1,69 @@
+package tree;
+
+import java.util.*;
+
+public class q199 {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static List<Integer> rightSideView(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode node = queue.poll();
+
+               
+                if (i == size - 1) {
+                    result.add(node.val);
+                }
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+     
+        TreeNode root = new TreeNode(1);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(4);
+
+        List<Integer> answer = rightSideView(root);
+
+        System.out.println(answer);
+    }
+}
